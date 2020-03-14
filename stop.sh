2acc40d13092ed5b1825dc9cff91505e90f4e736
IceBurst <TimTheEngineer@gmail.com>
2020-03-14 13:46:52 -0400

    stop.sh - pid tracking
    adding advanced pid tracking for shutdown

--- stop.sh
+++ stop.sh
@@ -1,10 +1,26 @@
 #!/bin/sh
 
+NC='\033[0m' # No Color
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+
+# Track the pid if we can find it
+read pid 2>/dev/null <run.pid
+valid_pid=$?
+
 echo 'Calling GET /admin/stop on local Qortal node'
 if curl --url http://localhost:12391/admin/stop 1>/dev/null 2>&1; then
-	echo "Qortal node responded and should be shutting down"
-	exit 0
+        echo "Qortal node responded and should be shutting down"
+        if [ $valid_pid -eq 0 ]; then
+                echo "Monitoring for Qortal to end"
+                tail --pid=${pid} -f /dev/null
+                rm run.pid
+                echo "${GREEN}Qortal ended gracefully"
+                echo "${NC}"
+        fi
+        exit 0
 else
-	echo "No response from Qortal node - not running?"
-	exit 1
+        echo "${RED}No response from Qortal node - not running?"
+        echo "${NC}"
+        exit 1
 fi
